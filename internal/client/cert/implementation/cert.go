85d4004cdffe9eecda660ffd911b5ddc71384b84
Dima191 <koshkin_mitya25@mail.ru>
2023-11-24 16:07:13 +0300

    Create client for obtaining TLS certificates
    - Implement CertClient struct with methods to fetch TLS certificates

---
+++ internal/client/cert/implementation/cert.go
@@ -0,0 +1,31 @@
+package certclientimpl
+
+import (
+	"context"
+	"errors"
+	pb "github.com/Dima191/cert-server/pkg/cert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log/slog"
+	xdsmodels "xds_server/internal/models"
+)
+
+func (c *client) Cert(ctx context.Context, domain string) (*xdsmodels.Cert, error) {
+	res, err := c.api.Cert(ctx, &pb.CertRequest{
+		Domain: domain,
+	})
+	if err != nil {
+		if status.Convert(err).Code() == codes.NotFound {
+			c.logger.Info("no certificate data", slog.String("domain", domain))
+			return nil, errors.New("no certificate data")
+		}
+		c.logger.Error("failed to get cert", slog.String("domain", domain), slog.String("error", err.Error()))
+		return nil, err
+
+	}
+
+	return &xdsmodels.Cert{
+		CertificateChain: string(res.CertificateChain),
+		PrivateKey:       string(res.PrivateKey),
+	}, nil
+}
