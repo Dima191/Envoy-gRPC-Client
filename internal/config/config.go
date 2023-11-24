6d5af44faee809d7a881e980297627a3745be0cc
Dima191 <koshkin_mitya25@mail.ru>
2023-11-24 13:52:23 +0300

    Implement configuration reading from config file
    - Add Config structure to handle configuration loading
    - Support for reading from YAML files

---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package xdsconfig
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"sync"
+)
+
+type Config struct {
+	CertServerPort             int    `yaml:"cert_server_port" env-required:"true"`
+	RouteServerPort            int    `yaml:"route_server_port" env-required:"true"`
+	EndpointManagementPort     int    `yaml:"endpoint_management_port" env-required:"true"`
+	XdsPort                    int    `yaml:"xds_port" env-required:"true"`
+	EnvoyProtectedListenPort   uint32 `yaml:"envoy_protected_listen_port" env-required:"true"`
+	EnvoyUnprotectedListenPort uint32 `yaml:"envoy_unprotected_listen_port" env-required:"true"`
+}
+
+var once sync.Once
+
+func New(configPath string) (*Config, error) {
+	var err error
+	cfg := Config{}
+	once.Do(func() {
+		err = cleanenv.ReadConfig(configPath, &cfg)
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
