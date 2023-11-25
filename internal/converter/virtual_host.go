2dc89564703382660bd57f60923152aafe304230
Dima191 <koshkin_mitya25@mail.ru>
2023-11-25 10:29:44 +0300

    Add internal converter from DomainEndpoint model to VirtualHost model

---
+++ internal/converter/virtual_host.go
@@ -0,0 +1,33 @@
+package xdsconverter
+
+import (
+	"strings"
+	xdsmodels "xds_server/internal/models"
+)
+
+const (
+	route = "route"
+)
+
+func VirtualHostConverter(domainEp *xdsmodels.DomainEndpoint) xdsmodels.VirtualHost {
+	var clusterName strings.Builder
+	clusterName.WriteString(cluster)
+	clusterName.WriteString("-")
+	clusterName.WriteString(domainEp.Domain)
+
+	var routeName strings.Builder
+	routeName.WriteString(route)
+	routeName.WriteString("-")
+	routeName.WriteString(domainEp.Domain)
+
+	vh := xdsmodels.VirtualHost{
+		Domain: domainEp.Domain,
+		Route: &xdsmodels.Route{
+			Name:        routeName.String(),
+			Prefix:      "/",
+			ClusterName: clusterName.String(),
+		},
+	}
+
+	return vh
+}
